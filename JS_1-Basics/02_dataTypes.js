c47a47acff0b375f398725c9c91513e110d87d24
Bhawani_K <110911568+Bhawani-K@users.noreply.github.com>
2023-05-07 16:25:09 +0000

    concepts_DataTypes

---
+++ JS_1-Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treats all jd Code as per new version
+
+// alert(3+3)  // we are using NodeJs not browser, This works in browser becoz there it gets binded in the document.
+
+let name="Bhawani_K"
+let age= 30
+let isloggedIn=false
+let state;
+
+/* 
+    primitive DataTypes 
+*/
+// number => 2 to power 53
+// bigInt
+// String => "" or ''
+// Boolean  => true / false
+// null => standalone value / empty
+// undefined => declared but not initialized
+// Symbol => to find the unique
+
+
+/* 
+    Non-Primitive DataTypes
+*/
+// Object
+// Array
+
+console.log(typeof age);
+console.log(typeof null); // Object => 
+console.log(typeof undefined); // undefined
+
